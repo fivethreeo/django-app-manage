2d638db46e447131bc5dd8310abecaae4daefc57
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2015-09-30 14:02:47 +0200

    add some commas here

--- app_manage/defaults.py
+++ app_manage/defaults.py
@@ -39,11 +39,11 @@ PASSWORD_HASHERS = [
 ALLOWED_HOSTS = ['localhost']
 
 MIDDLEWARE_CLASSES = [
-    'django.contrib.sessions.middleware.SessionMiddleware'
-    'django.middleware.common.CommonMiddleware'
-    'django.middleware.csrf.CsrfViewMiddleware'
-    'django.contrib.auth.middleware.AuthenticationMiddleware'
-    'django.contrib.messages.middleware.MessageMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
